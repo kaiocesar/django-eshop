76ae16e5140a77a9c134503e13d9d7425b27e278
Leandro <tecnico.kaio@gmail.com>
2015-12-04 02:42:19 -0200

    models: customer, products

--- customers/models.py
+++ customers/models.py
@@ -2,4 +2,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Customers(models.Model):
+    name = models.CharField(max_length=30, verbose_name="Name")
+    cpf = models.CharField(max_length=14, verbose_name="CPF")
+    rg = models.CharField(max_length=20, verbose_name="RG")
+    email = models.CharField(max_length=50, verbose_name="E-mail")
+    address = models.CharField(max_length=60, verbose_name="Address")
+    street = models.CharField(max_length=50, verbose_name="Street")
+    neighborhood = models.CharField(max_length=50, verbose_name="Neighborhood")
+    city = models.CharField(max_length=50, verbose_name="City")
+    phone = models.CharField(max_length=12, verbose_name="Phone")
+    cellphone = models.CharField(max_length=12, verbose_name="Cellphone")
+    description = models.TextField(verbose_name="Description")
+    avatar = models.TextField(verbose_name="Avatar")
+
--- products/models.py
+++ products/models.py
@@ -2,4 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Products(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Product name")
+    description = models.TextField(verbose_name="Description")
+    created_at = models.DateTimeField(verbose_name="Created at")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Price")
