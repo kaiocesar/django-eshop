a273efdc9beb65e88f57ca5ed292d9e2df8548a1
Leandro <tecnico.kaio@gmail.com>
2015-12-04 03:07:15 -0200

    category product

--- products/models.py
+++ products/models.py
@@ -3,7 +3,31 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Products(models.Model):
+
+    STATUS_CHOICE = (
+        (True, 'Active'),
+        (False, 'Inactive')
+    )
+
     name = models.CharField(max_length=50, verbose_name="Product name")
     description = models.TextField(verbose_name="Description")
     created_at = models.DateTimeField(verbose_name="Created at")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Price")
+    amount = models.IntegerField(verbose_name="Amount")
+    discount = models.IntegerField(verbose_name="Discount")
+    status = models.BooleanField(verbose_name="Status", choices=STATUS_CHOICE)
+
+
+
+class ProductPhotos(models.Model):
+    photografy = models.CharField(max_length=150)
+    product = models.ForeignKey(Products)
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Category name")
+
+
+class CategoriesProducts(models.Model):
+    product = models.ForeignKey(Products, verbose_name="Product code")
+    category = models.ForeignKey(Categories, verbose_name="Category code")
--- sales/models.py
+++ sales/models.py
@@ -2,4 +2,28 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class PaymentsMethods(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+
+class Sales(models.Model):
+    code = models.CharField(max_length='40', verbose_name="Code sales")
+    status = models.CharField(max_length='50', verbose_name="Status")
+    payment_method = models.ForeignKey(PaymentsMethods, verbose_name="Payment Method")
+    customer = models.ForeignKey('customers.Customers', "Customer")
+
+
+
+class SaleProducts(models.Model):
+    product = models.ForeignKey('products.Products', verbose_name="Product code")
+    sales = models.ForeignKey(Sales, verbose_name="Sales code")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Price")
+    amount = models.IntegerField()
+    discount = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+
